f5a669c3c4b2e2da937ae44a6b6c449bac8d377b
zmzss-10 <zmzss2278@gmail.com>
2025-04-10 19:58:41 +0800

    Delete Initialize.m

--- Initialize.m
+++
@@ -1,6 +0,0 @@
-function UserInfoList = Initialize(numUsers,queueLength)
-for i=1:numUsers
-    UserInfoList(i).packetNum=0;%第i个节点待发送的数据包总数
-    UserInfoList(i).packets=zeros(2,queueLength);%各个包的信息第一行为到达时间，第二行为实际发送时间，超过队列的数据包丢弃
-end
-
